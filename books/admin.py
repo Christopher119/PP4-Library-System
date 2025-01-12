58629c933b75aa22d356201249b0fd858b8f76ee
Christopher Dunne <christopherdunne@outlook.ie>
2025-01-12 19:48:01 +0000

    Add: summernote to enhance admin view

--- books/admin.py
+++ books/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from .models import Book, Review
+from django_summernote.admin import SummernoteModelAdmin
+
+# registering a class of BookAdmin
+@admin.register(Book)
+class BookAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'author', 'copies_available')
+    search_fields = ['title', 'author', 'genre', 'copies_available']
+    list_filter = ('status', 'author', 'genre',)
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('description',)
 
 # Register your models here.
-admin.site.register(Book)
 admin.site.register(Review)
--- pp4codelibrary/urls.py
+++ pp4codelibrary/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('books/', include("books.urls"), name='books-urls'),
     path('admin/', admin.site.urls),
+    path('books/', include("books.urls"), name='books-urls'),
+    path('summernote/', include('django_summernote.urls')),
 ]
