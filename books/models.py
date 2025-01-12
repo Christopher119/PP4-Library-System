9cdd379f6a9032eff187ee124e3aff7900518692
Christopher Dunne <christopherdunne@outlook.ie>
2025-01-12 19:07:39 +0000

    Add: Review model to database.

--- books/models.py
+++ books/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 
 # constant to determine whether there are any remaining books to be reserved by users
 STATUS = ((0, "Unavailable"), (1, "Available"))
+APPROVAL = ((0, "Awaiting Moderation"), (1, "Approved"))
 
 # Create your models here.
 
@@ -25,3 +26,23 @@ class Book(models.Model):
 
     # attribute to set books as available by default
     status = models.IntegerField(choices=STATUS, default=1)
+
+class Review(models.Model):
+    """A Model for each Review in the database."""
+    # attribute to get the user who left the review
+    # also deletes all reviews by user when user is deleted
+    reader = models.ForeignKey(
+                                User, on_delete=models.CASCADE, 
+                                related_name="reviewer"
+                                )
+    # attribute to get the book the user is reviewing
+    # also deletes all reviews on the book if it is deleted                        
+    reviewed_book = models.ForeignKey(
+                                Book, on_delete=models.CASCADE, 
+                                related_name="reviews"
+                                )
+    # description of the book
+    # not unique as some users may leave similar reviews
+    review_content = models.TextField()
+    # attribute to set review as unavailable by default
+    approved = models.IntegerField(choices=APPROVAL, default=0)
