9f8223395d1e0a1b1d53296f80dbd3516cc3820c
Christopher Dunne <christopherdunne@outlook.ie>
2025-01-12 23:15:09 +0000

    Add: POST review functionality

--- books/views.py
+++ books/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Book
+from .forms import ReviewForm
 
 # Create your views here.
 
@@ -31,12 +32,25 @@ def book_detail(request, slug):
 
     reviews = book.reviews.all()
     review_count = book.reviews.filter(approved=True).count()
+    review_form = ReviewForm()
+
+    if request.method == "POST":
+        review_form = ReviewForm(data=request.POST)
+
+    if review_form.is_valid():
+        review = review_form.save(commit=False)
+        review.reader = request.user
+        review.reviewed_book = book
+        review.save()
+
+    review_form = ReviewForm()
 
     return render(
         request,
         "books/book_detail.html",
         {"book": book,
         "reviews": reviews,
-        "review_count": review_count,},
+        "review_count": review_count,
+        "review_form": review_form,},
         
     )
