d0dc0e0256f2825d7b2b5a11b8ee60b48cf35ad0
Christopher Dunne <christopherdunne@outlook.ie>
2025-01-13 08:29:24 +0000

    Fix: Pathing issues resolved.

--- pp4codelibrary/settings.py
+++ pp4codelibrary/settings.py
@@ -27,7 +27,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6bex0drve2hkdtlw8oqk-(-hqglvt#d#&46kr6n0ix6c^wc&ne'
+SECRET_KEY = '54TCcQuKZ7obgFz5Hl6t4z8vyQcMALAY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
